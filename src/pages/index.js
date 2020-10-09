7483d6d4da49745f92f91f58589b949211156399
Michael Nunes <tinysquid.git@gmail.com>
2020-10-08 21:08:42 -0700

    Tweak seo again

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Layout from "../components/layout/layout";
 
+import SEO from "../components/seo";
+
 export default function Home() {
 	return (
-		<Layout>
-			<p>Hello world!</p>
-		</Layout>
+		<>
+			<SEO />
+			<Layout>
+				<p>Hello world!</p>
+			</Layout>
+		</>
 	);
 }
