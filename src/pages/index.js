8a0da0a52016df33f0a591d92961c0832d1f00ee
Michael Nunes <tinysquid.git@gmail.com>
2020-10-13 11:31:19 -0700

    Add page header and style per design

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,24 +16,27 @@ export default function Home() {
 			<SEO />
 			<Layout>
 				<div className="container">
-					<Card
-						position="left"
-						title="Basic"
-						price="19.99"
-						items={["500 GB Storage", "2 Users Allowed", "Send up to 3GB"]}
-					/>
-					<Card
-						primary
-						title="Professional"
-						price="24.99"
-						items={["1 TB Storage", "5 Users Allowed", "Send up to 10GB"]}
-					/>
-					<Card
-						position="right"
-						title="Master"
-						price="39.99"
-						items={["2 TB Storage", "10 Users Allowed", "Send up to 20GB"]}
-					/>
+					<h1 className="price-header">Our Pricing</h1>
+					<div className="card-container">
+						<Card
+							position="left"
+							title="Basic"
+							price="19.99"
+							items={["500 GB Storage", "2 Users Allowed", "Send up to 3GB"]}
+						/>
+						<Card
+							primary
+							title="Professional"
+							price="24.99"
+							items={["1 TB Storage", "5 Users Allowed", "Send up to 10GB"]}
+						/>
+						<Card
+							position="right"
+							title="Master"
+							price="39.99"
+							items={["2 TB Storage", "10 Users Allowed", "Send up to 20GB"]}
+						/>
+					</div>
 				</div>
 			</Layout>
 		</>
