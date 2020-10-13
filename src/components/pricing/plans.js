b49f37aabdb4731069e7a1e2e7bae3dd2d582714
Michael Nunes <tinysquid.git@gmail.com>
2020-10-13 13:02:36 -0700

    Move plan data to it's own file for easy modification

---
+++ src/components/pricing/plans.js
@@ -0,0 +1,26 @@
+export default {
+	basic: {
+		title: "Basic",
+		price: {
+			monthly: 19.99,
+			annually: 199.99,
+		},
+		benefits: ["500 GB Storage", "2 Users Allowed", "Send up to 3GB"],
+	},
+	professional: {
+		title: "Professional",
+		price: {
+			monthly: 24.99,
+			annually: 249.99,
+		},
+		benefits: ["1 TB Storage", "5 Users Allowed", "Send up to 10GB"],
+	},
+	master: {
+		title: "Master",
+		price: {
+			monthly: 39.99,
+			annually: 399.99,
+		},
+		benefits: ["2 TB Storage", "10 Users Allowed", "Send up to 20GB"],
+	},
+};
